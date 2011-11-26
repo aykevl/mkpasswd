97be49a91f9f9528989ddd66998a629cb067d5bf
Ayke van Laethem <aykevanlaethem@gmail.com>
2011-11-26 22:12:46 +0100

    Added cache-manifest.php (was a symbolic link)

--- cache-manifest.php
+++ cache-manifest.php
@@ -1,1 +1,21 @@
-../cache-manifest.php
+<?php
+header('Content-type: text/cache-manifest');
+echo "CACHE MANIFEST\n";
+
+$dir = '.';
+
+$hashes = '';
+$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
+foreach($iterator as $file) {
+    if ($file->IsFile() &&
+        $file != './cache-manifest.php' &&
+        substr($file->getFilename(), 0, 1) != '.')
+    {
+        echo $file . "\n";
+        $hashes .= md5_file($file);
+    }
+}
+echo '# Hash: '.md5($hashes) . "\n";
+
+
+?>
